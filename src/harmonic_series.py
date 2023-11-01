80448b0629c20ad8db771fcd87ff386efc58819b
Victor-DM-cpu <58991915+Deaconu-Victor-M@users.noreply.github.com>
2023-11-01 22:17:57 +0200

    fix: comments

--- src/harmonic_series.py
+++ src/harmonic_series.py
@@ -4,6 +4,15 @@ from texttable import Texttable
 
 # Function to compute the sum of the series
 def series_of_sum_numerically(n):
+    """
+    This function computes the sum of the series numerically in its original form
+
+    Args:
+        n (int): The number of terms to be considered
+
+    Returns:
+        float: The sum of the series
+    """
     sum = 0
     for i in range(1, n+1):
         sum += ((-1)**(i+1))/i
@@ -11,7 +20,19 @@ def series_of_sum_numerically(n):
 
 
 # Function to compute the rearranged sum
-def rearranging_sum(p, q, left_off_positive, left_off_negative):
+def sum_segment(p, q, left_off_positive, left_off_negative):
+    """
+    This function computes the sum of the series in the rearranged form
+
+    Args:
+        p (int): The number of positive terms for each iteration
+        q (int): The number of negative terms for each iteration
+        left_off_positive (int): Shows Where the sum was left off for the positive terms
+        left_off_negative (int): Shows Where the sum was left off for the negative terms
+
+    Returns:
+        float, int, int: The sum of the series in the rearranged order, The last positive term and the last negative term considered in the sum
+    """
 
     sum_p_positive = 0
     for i in range(left_off_positive, left_off_positive + 2*p - 1, 2):
@@ -32,7 +53,7 @@ def rearranged_sum(n, p, q):
     total_sum = 0
     while left_off_negative <= n and left_off_positive <= n:
         partial_sum = 0
-        partial_sum, pos_temp, neg_temp = rearranging_sum(p, q, left_off_positive, left_off_negative)
+        partial_sum, pos_temp, neg_temp = sum_segment(p, q, left_off_positive, left_off_negative)
         total_sum += partial_sum
         left_off_positive = pos_temp
         left_off_negative = neg_temp
@@ -45,7 +66,7 @@ def main():
     q = int(input("Please enter no of q negative terms: "))
 
     table = Texttable()
-    table.add_rows([["n", "Sum of the series", "ln 2", "Rearranged sum"], [n, f"~{series_of_sum_numerically(n)}", f"~{math.log(2)}", f"~{rearranged_sum(n, p, q)}"]])
+    table.add_rows([["n", "ln 2", "Sum of the series", "Rearranged sum"], [n, f"~{math.log(2)}", f"~{series_of_sum_numerically(n)}", f"~{rearranged_sum(n, p, q)}"]])
     print(table.draw())
 
 main()
